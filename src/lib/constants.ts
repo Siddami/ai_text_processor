3ce4ec0bf6bc3c52f13f0f14736b8a5220404a61
Siddami <dharmmy621@gmail.com>
2025-02-21 19:47:25 +0100

    feat: add langauge data in constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+import { Language } from "@/types";
+
+export const languages: Language[] = [
+    { value: 'en', label: 'English' },
+    { value: 'pt', label: 'Portuguese' },
+    { value: 'es', label: 'Spanish' },
+    { value: 'ru', label: 'Russian' },
+    { value: 'tr', label: 'Turkish' },
+    { value: 'fr', label: 'French' },
+];
